9d91b12c8136283d1121c40751b1d7bfdce3d57b
AnasRizwan1 <chanasrizwan@gmail.com>
2025-05-09 21:23:53 +0500

    logo and welcomepage

--- SmartPropertyManagementSystem/src/main/java/com/example/smartpropertymanagementsystem/LoginPage.java
+++ SmartPropertyManagementSystem/src/main/java/com/example/smartpropertymanagementsystem/LoginPage.java
@@ -1,6 +1,7 @@
 
 package com.example.smartpropertymanagementsystem;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
