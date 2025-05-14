93418ff267072482fb5221f8112b841a385d9450
Muhammad Ramis Khan <mukhan.bscs24seecs@seecs.edu.pk>
2025-05-14 20:27:01 +0500

    Beta version

--- SmartPropertyManagementSystem/src/main/java/com/example/smartpropertymanagementsystem/AddAdvance.java
+++ SmartPropertyManagementSystem/src/main/java/com/example/smartpropertymanagementsystem/AddAdvance.java
@@ -3,14 +3,14 @@ package com.example.smartpropertymanagementsystem;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import java.sql.*;
 import java.io.IOException;
 
 public class AddAdvance implements sceneToDashboard, clearInfo, saveInfo {
     @FXML TextField plotNoTextField;
-    @FXML TextField amountTextField;
-    @FXML TextField receivingDateTextField;
+    @FXML DatePicker receivingDateTextField;
     @FXML TextField paidAmountTextField;
     @FXML Button saveButton;
     @FXML Button clearButton;
@@ -21,10 +21,7 @@ public class AddAdvance implements sceneToDashboard, clearInfo, saveInfo {
 
     @Override
     public void clearInformation(ActionEvent event) {
-        plotNoTextField.clear();
-        amountTextField.clear();
-        receivingDateTextField.clear();
-        paidAmountTextField.clear();
+
     }
 
     @Override
